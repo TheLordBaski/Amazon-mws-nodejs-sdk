9c8cf4ee41ec43474374bd523a1635882d7eb76b
Jakub Gabčo <jakub.gabco@gmail.com>
2015-08-25 22:00:05 +0200

    first working request

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var config = require("./config.json");
 request
     .post(
     {
-        url: 'http://mws.amazonservices.co.uk/Orders/2013-09-01',
+        url: 'https://mws.amazonservices.com/Orders/2013-09-01',
         form: {
             'Action' : 'GetServiceStatus',
-            'SellerId' : 'A2JUH45EK8JPA4',
+            'SellerId' : config.SellerID,
             'SignatureVersion' : '2',
-            'Timestamp' : Date.now(),
+            'Timestamp' :'2015-08-25T19:56:52.538Z',
             'Version' : '2013-09-01',
             'Signature' : 'j4EGXEQ%2Bm5C99K9E6FG4HjDT8AxgNK77uhbvoZRx%2B8o%3D',
             'SignatureMethod' : 'HmacSHA256',

1395924718c5ce9dbebfeab9e696cf5cff065e7a
Jakub Gabčo <jakub.gabco@gmail.com>
2015-08-26 00:36:01 +0200

    Example how to use API for contributor Pecinka

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var mws = require("./lib/index.js");
+var config = require("./config.json");
 
-console.log(mws);
+
+mws.products.GetServiceStatus(config,{}, false, function(err, data){
+    console.log(data);
+});
+
+mws.sellers.GetServiceStatus(config,{}, true, function(err, data){
+    console.log(data);
+});
+

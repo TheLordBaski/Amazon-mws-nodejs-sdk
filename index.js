0a94292a07cbe117db8c8e0e7626ff7e68fcafa9
Jakub Gabčo <jakub.gabco@gmail.com>
2015-08-25 21:20:11 +0200

    First try of request

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var request = require('request');
+var config = require("./config.json");
 request
-    .get('http://google.com/img.png')
-    .on('response', function(response) {
-        console.log(response.statusCode) // 200
-        console.log(response.headers['content-type']) // 'image/png'
-    })
-    .pipe(request.put('http://mysite.com/img.png'))
+    .post(
+    {
+        url: 'http://mws.amazonservices.co.uk/Orders/2013-09-01',
+        form: {
+            'Action' : 'GetServiceStatus',
+            'SellerId' : 'A2JUH45EK8JPA4',
+            'SignatureVersion' : '2',
+            'Timestamp' : Date.now(),
+            'Version' : '2013-09-01',
+            'Signature' : 'j4EGXEQ%2Bm5C99K9E6FG4HjDT8AxgNK77uhbvoZRx%2B8o%3D',
+            'SignatureMethod' : 'HmacSHA256',
+            'AWSAccessKeyId' : config.AWSAccessKeyID
+        }
+    }, function (err, httpResponse, body) {
+        /* ... */
+        console.log(err);
+        console.log(httpResponse);
+        console.log(body);
+    });

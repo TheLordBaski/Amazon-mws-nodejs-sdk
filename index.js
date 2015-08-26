8d449e71564293991a5e5a8bd845c92ed48bfc73
Jakub Gabčo <jakub.gabco@gmail.com>
2015-08-26 16:01:19 +0200

    Parameter checking and throwing useless request

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ var config = require("./config.json");
 
 mws.products.GetServiceStatus(config, false, function(err, data){
     console.log(data);
-});
+} );
 
-mws.sellers.ListMarketplaceParticipations(config, true, function(err, data){
-    console.log(data);
-});
+/*mws.sellers.ListMarketplaceParticipationsByNextToken(config, true,{}, function(err, data){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data);
+    }
+});*/
 

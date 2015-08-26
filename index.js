e53de969c6793fe2763a562301c991ffa2e2f7b5
Jakub Gabčo <jakub.gabco@gmail.com>
2015-08-26 13:51:54 +0200

    Examples

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var mws = require("./lib/index.js");
 var config = require("./config.json");
 
 
-mws.products.GetServiceStatus(config,{}, false, function(err, data){
+mws.products.GetServiceStatus(config, false, function(err, data){
     console.log(data);
 });
 
-mws.sellers.GetServiceStatus(config,{}, true, function(err, data){
+mws.sellers.ListMarketplaceParticipations(config, true, function(err, data){
     console.log(data);
 });
 

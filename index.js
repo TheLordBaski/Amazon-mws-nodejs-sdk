8cf8c8204e4896f0ba1b41d0b553554b7ea71298
Zdenek Pecinka <zdeny.pecinka@gmail.com>
2015-09-09 23:03:01 +0200

    signature still not working

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var mws = require("./lib/index.js");
 var config = require("./config.json");
 
-//mws.products.ListMatchingProducts(config, {MarketplaceId: 'APJ6JRA9NG5V4', Query: 'ed',QueryContextId: 'Music'}, true, function (err, data) {
-//    console.log(data);
-//});
+mws.products.ListMatchingProducts(config, {MarketplaceId: 'APJ6JRA9NG5V4', Query: 'a',QueryContextId: 'Music'}, true, function (err, data) {
+    console.log(data);
+});
 
 
-mws.products.GetMatchingProduct(config, {
+/*mws.products.GetMatchingProduct(config, {
         MarketplaceId: 'APJ6JRA9NG5V4',
         ASINList: {
             'ASINList.ASIN.1': 'B00012SYY6', 'ASINList.ASIN.2': 'B00004CZ0F'
@@ -14,4 +14,4 @@ mws.products.GetMatchingProduct(config, {
     }, true, function (err, data) {
         console.log(data);
     }
-);
+);*/
